669442406cc2226671cdb3a648e0ec66b4c9c894
Joshua Ombasa <joshuaombasa@gmail.com>
2022-12-24 22:28:20 +0300

    created routes to view notes

--- app.js
+++ app.js
@@ -9,17 +9,52 @@ app.set('view engine', 'ejs')
 // source for static 
 app.use(express.static('public'))
 
+const notes = [
+    {
+        id: 1,
+        title: 'Title 1',
+        body: 'Body of the first note goes here'
+    },
+    {
+        id: 2,
+        title: 'Title 2',
+        body: 'Body of the second note goes here'
+    },
+    {
+        id: 3,
+        title: 'Title 3',
+        body: 'Body of the third note goes here'
+    },
+    {
+        id: 4,
+        title: 'Title 4',
+        body: 'Body of the fourth note goes here'
+    },
+    {
+        id: 5,
+        title: 'Title 5',
+        body: 'Body of the fifth note goes here'
+    }
+]
 // routestocu
 // homepage
 app.get('/', (req, res) => {
-res.render('index', {title: 'hello world - from backend'})
+res.render('index')
 })
 
-// about
-app.get('/about', (req, res) => {
-    const languages = ['Java', 'Go', 'PHP', 'JavaScript', 'Python', 'C++', 'Rust']
-    res.render('about',{languages})
+// view all notes
+app.get('/notes', (req, res) => {
+    
+    res.render('notes',{notes})
 })
+
+// view a single note
+app.get('/note/:id', (req, res) => {
+    // get the note
+    const note = notes.find(note => note.id === parseInt(req.params.id))
+    res.render('note', {note})
+})
+
 
 // signup
 app.get('/signup', (req, res) => {
