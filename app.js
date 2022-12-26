f415191756c8d0e349a6420825ddaf8a8f62efb2
Joshua Ombasa <joshuaombasa@gmail.com>
2022-12-26 22:13:09 +0300

    connect db and ran quesries

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 import express from 'express'
+import mysql from 'mysql'
 
 const app = express()
+
+// create a db connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'kezo'
+})
 
 // set template engine and specify extension
 
@@ -9,33 +18,9 @@ app.set('view engine', 'ejs')
 // source for static 
 app.use(express.static('public'))
 
-const notes = [
-    {
-        id: 1,
-        title: 'Title 1',
-        body: 'Body of the first note goes here'
-    },
-    {
-        id: 2,
-        title: 'Title 2',
-        body: 'Body of the second note goes here'
-    },
-    {
-        id: 3,
-        title: 'Title 3',
-        body: 'Body of the third note goes here'
-    },
-    {
-        id: 4,
-        title: 'Title 4',
-        body: 'Body of the fourth note goes here'
-    },
-    {
-        id: 5,
-        title: 'Title 5',
-        body: 'Body of the fifth note goes here'
-    }
-]
+// configuration to accept form information
+app.use(express.urlencoded({extended: true}))
+
 // routestocu
 // homepage
 app.get('/', (req, res) => {
@@ -44,22 +29,53 @@ res.render('index')
 
 // view all notes
 app.get('/notes', (req, res) => {
+    // retrieve all notes from notes table
+    let sql = 'SELECT * FROM notes'
+    connection.query(
+        sql, (error, results) => {
+            res.render('notes', {notes: results})
+        }
+    )
     
-    res.render('notes',{notes})
 })
 
 // view a single note
 app.get('/note/:id', (req, res) => {
-    // get the note
-    const note = notes.find(note => note.id === parseInt(req.params.id))
-    res.render('note', {note})
+    // retrieve note from notes
+    let sql = 'SELECT * FROM notes WHERE id = ?'
+    connection.query(
+        sql,
+         [parseInt(req.params.id)],
+         (error, results) => {
+            res.render('note', {note:results[0]})
+        }
+    )
+    
 })
 
 
-// signup
-app.get('/signup', (req, res) => {
-    res.render('signup')
+// display create a note form
+app.get('/create', (req, res) => {
+    res.render('create')
 })
+
+// submit ceate a note form
+app.post('/create', (req, res)=> {
+    const note = {
+        title: req.body.title,
+        body: req.body.body
+
+    }
+    let sql = 'INSERT INTO notes (title, body) VALUES (?, ?)'
+    connection.query(
+        sql,
+        [note.title, note.body],
+        (error, results) => {
+            res.redirect('/notes')
+        }
+    )
+}
+)
 
 // 404 error
 app.get('*', (req, res) => {
